9cd15d698aaa52e419372e735c4c73b9cf4d1502
elysium09 <eyueldaniel0921@gamil.com>
2022-03-21 21:48:35 +0300

    basic ui

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaQuestionCircle, FaTicketAlt } from 'react-icons/fa';
 
 function Home() {
-    return <div>Home</div>;
+    return (
+        <>
+            <section className='heading'>
+                <h1>what do you need help with?</h1>
+                <p>please choose from the options below</p>
+            </section>
+            <Link to='/new-ticket' className='btn btn-reverse btn-block'>
+                <FaQuestionCircle /> Create new ticket
+            </Link>
+            <Link to='/tickets' className='btn btn-block'>
+                <FaTicketAlt /> View my tickets
+            </Link>
+        </>
+    );
 }
 
 export default Home;
