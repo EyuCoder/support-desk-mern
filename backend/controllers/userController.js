85ed99438601336f2f28d425e809a7084be13fe4
elysium09 <eyueldaniel0921@gamil.com>
2022-03-21 16:41:44 +0300

    'userRegister'

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,7 @@
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/userModel');
 /**
  * @desc    Register new User
  * @route   /api/users
@@ -12,6 +15,37 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error('Please include all fields');
   }
+
+  //Check if user already exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already Exists.');
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create new User
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+
   res.send('Register Route');
 });
 
