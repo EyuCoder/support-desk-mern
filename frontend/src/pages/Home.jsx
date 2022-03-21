66168b8bad6111a40f7e432f653371224e7f8509
elysium09 <eyueldaniel0921@gamil.com>
2022-03-21 21:58:00 +0300

    tab size

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,20 +2,20 @@ import { Link } from 'react-router-dom';
 import { FaQuestionCircle, FaTicketAlt } from 'react-icons/fa';
 
 function Home() {
-    return (
-        <>
-            <section className='heading'>
-                <h1>what do you need help with?</h1>
-                <p>please choose from the options below</p>
-            </section>
-            <Link to='/new-ticket' className='btn btn-reverse btn-block'>
-                <FaQuestionCircle /> Create new ticket
-            </Link>
-            <Link to='/tickets' className='btn btn-block'>
-                <FaTicketAlt /> View my tickets
-            </Link>
-        </>
-    );
+  return (
+    <>
+      <section className='heading'>
+        <h1>what do you need help with?</h1>
+        <p>please choose from the options below</p>
+      </section>
+      <Link to='/new-ticket' className='btn btn-reverse btn-block'>
+        <FaQuestionCircle /> Create new ticket
+      </Link>
+      <Link to='/tickets' className='btn btn-block'>
+        <FaTicketAlt /> View my tickets
+      </Link>
+    </>
+  );
 }
 
 export default Home;
