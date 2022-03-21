c7d959399eff18e665a98dae56805b7a8a1ed21a
elysium09 <eyueldaniel0921@gamil.com>
2022-03-21 15:05:27 +0300

    basic backend setup

---
+++ backend/controllers/userController.js
@@ -0,0 +1,30 @@
+const asyncHandler = require("express-async-handler");
+/**
+ * @desc    Register new User
+ * @route   /api/users
+ * @access  public
+ */
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  //validation
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please include all fields");
+  }
+  res.send("Register Route");
+});
+
+/**
+ * @desc    Login a User
+ * @route   /api/users/login
+ * @access  public
+ */
+const loginUser = asyncHandler(async (req, res) => {
+  res.send("Login Route");
+});
+
+module.exports = {
+  registerUser,
+  loginUser,
+};
