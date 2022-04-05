e2b03a88073c9bd1577fac84f9d5f09d4c7ffee4
elysium09 <eyueldaniel0921@gamil.com>
2022-04-05 16:58:35 +0300

    getticket

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -26,6 +26,22 @@ export const createTicket = createAsyncThunk(
     }
 )
 
+// get user tickets
+export const getTickets = createAsyncThunk(
+    'tickets/getAll',
+    async (_, thunkAPI) => {
+        try {
+            const token = thunkAPI.getState().auth.user.token
+            return await ticketService.getTickets(token)
+        } catch (error) {
+            const message =
+                (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+
+            return thunkAPI.rejectWithValue(message)
+        }
+    }
+)
+
 export const ticketSlice = createSlice({
     name: 'ticket',
     initialState,
@@ -33,14 +49,28 @@ export const ticketSlice = createSlice({
         reset: (state) => initialState
     },
     extraReducers: (builder) => {
-        builder.addCase(createTicket.pending, (state) => {
-            state.isLoading = true
-        })
+        builder
+            .addCase(createTicket.pending, (state) => {
+                state.isLoading = true
+            })
             .addCase(createTicket.fulfilled, (state) => {
                 state.isLoading = false
                 state.isSuccess = true
             })
             .addCase(createTicket.rejected, (state, action) => {
+                state.isLoading = false
+                state.isError = true
+                state.message = action.payload
+            })
+            .addCase(getTickets.pending, (state) => {
+                state.isLoading = true
+            })
+            .addCase(getTickets.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.isSuccess = true
+                state.tickets = action.payload
+            })
+            .addCase(getTickets.rejected, (state, action) => {
                 state.isLoading = false
                 state.isError = true
                 state.message = action.payload
